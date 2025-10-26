585fcefe58e9865dc8376505119649c86bc0dcde
lanamaiumy <alana.maiumy@gmail.com>
2025-10-26 16:42:20 -0300

    feat: Implement pagination and enhancement CRUD

--- tests/Unit/Controllers/ControllerTestCase.php
+++ tests/Unit/Controllers/ControllerTestCase.php
@@ -7,12 +7,21 @@ use Tests\TestCase;
 
 abstract class ControllerTestCase extends TestCase
 {
-    protected function get(string $action, string $controller): string|false
+    /**
+     * @param array<string, mixed> $params
+     */
+    protected function get(string $action, string $controller, array $params = []): string|false
     {
         ob_start();
         $controllerInstance = new $controller();
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_REQUEST = $params;
+
         $request = new Request();
         $controllerInstance->$action($request);
+
+        $_REQUEST = [];
         return ob_get_clean();
     }
 }
